ee81cdfd5e8f0e085dd9ea64879b06bd4f78b3a4
anelso38 <anelso38@uncc.edu>
2018-02-20 22:46:38 -0500

    Update mostTweetsPerTimeline.py

--- mostTweetsPerTimeline.py
+++ mostTweetsPerTimeline.py
@@ -1,6 +1,6 @@
 def mostTweetsPerTimeline():
     INPUTFILE = input("Enter the file path: ")
-    INPUT_FILE_PATH = 'C:/Users/##/Desktop/' + INPUTFILE + '.txt' ##ADD USER FILEPATH
+    INPUT_FILE_PATH = 'C:/Users/##/Desktop/' + INPUTFILE + '.txt' ##ADD USER FILE PATH
 
     with open (INPUT_FILE_PATH, encoding = "latin-1") as myFile:
         twitter=myFile.readlines()
@@ -14,7 +14,7 @@ def mostTweetsPerTimeline():
             l[fileTemp[0]] = 1
     l = sorted(l.items(), key = operator.itemgetter(1), reverse = True)
  
-    outputFile = open('C:/Users/##/Desktop/mostTweetsPerTimeline.txt', 'w', encoding = "utf-8") ##ADD USER FILEPATH
+    outputFile = open('C:/Users/##/Desktop/mostTweetsPerTimeline.txt', 'w', encoding = "utf-8") ##ADD USER FILE PATH
     outputFile.write("The top 10 users who have tweeted the most for the entire timeline: \n")
     for i in range (0,10):
         outputFile.write("The user " + l[i][0] + " tweeted " + str(l[i][1]) + " times" + "\n")
