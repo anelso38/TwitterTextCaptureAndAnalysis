d889626e7a0eac32e10b08384751a372c8fd4016
anelso38 <anelso38@uncc.edu>
2018-04-10 14:35:35 -0400

    Update mostTweetsPerHour.py

--- mostTweetsPerHour.py
+++ mostTweetsPerHour.py
@@ -1,51 +1,50 @@
 import string
 import operator
 import sys
+import csv
 
 def mostTweetsPerHour():
-    INPUTFILE = input("Enter the file path: ")
-    INPUT_FILE_PATH = 'C:/Users/##/Desktop/' + INPUTFILE + '.txt' ##ADD USER FILE PATH
-
-    with open (INPUT_FILE_PATH, encoding = "latin-1") as myFile:
-        data=myFile.readlines()
-
-    l = {}
+    FilePath = 'C:/Users/Alex Nelson/Desktop/twitterData.txt' 
+    infoHours = []
+    with open (FilePath, encoding = "latin-1") as twitterFile:
+        tweets = twitterFile.readlines()
+          
+    output = []
     
-    for dat in data:
-        fileTemp = dat.split()
-        fileTemp2 = fileTemp[1].split(":")
-        twitterTemp = fileTemp[0] + " " + fileTemp2[1]
-        if twitterTemp in l:
-            l[twitterTemp]+=1
-        else:
-            l[twitterTemp]=1
-    l = sorted(l.items(), key = operator.itemgetter(1), reverse = True)
-
-    l2={}
-
-    totalNumPostsInFile = 0
-    for dat in l:
-        totalNumPostsInFile+=1
+    for tweet in tweets:
+        tweetInfo = tweet.split(' ')
+        date = tweetInfo[1].split(":")
+        hour = date[-3]
+        usernames = tweetInfo[0]
+        output.append(usernames)
+        userInfo = (usernames, hour)
+        infoHours.append(userInfo)
         
-        if(dat[0].split()[1]) in l2:
-            l2[dat[0].split()[1]]+=1
-        else:
-            l2[dat[0].split()[1]]=1
-
-    l2 = sorted(l2.items(), key = operator.itemgetter(1))
-    totalEntriesToPrint = 10*len(l2)
-    outputFile = open('C:/Users/##/Desktop/mostTweetsPerHour.txt', 'w', encoding = "utf-8") ##ADD USER FILE PATH
-
-    for x in range (0,len(l2)):
-        mSearch = 10
+    users = []
+    for user in output:
+        if user not in users:
+            users.append(user)
+                  
+          
+    mostTweets= []
+    for u in users:
+        for i in infoHours:
+            if i[0] == u:
+                count = infoHours.count(i)
+                mostTweets.append(count)
+            list = (i,count)
+            mostTweets.append(list)
         
-        for dat in l:
-            
-            if mSearch == 0:
-                break
-            if dat[0].split()[1] == l2[x][0]:
-                outputFile.write("Username: " + dat[0].split()[0] + "\n Hour: " + l2[x][0] +"\n")
-                mSearch-=1
-    outputFile.close
+    print(mostTweets)
+        
+        
+    sortedList = sorted(mostTweets, key=lambda tup:(tup[1]))
+    maxTen = (sortedList[-10:])
     
+    maxTweetsFile = open('C:/Users/Alex Nelson/Desktop/mostTweetsPerHour.csv', 'w')  
+    with maxTweetsFile:  
+        writer = csv.writer(maxTweetsFile)
+        for row in maxTen:
+            writer.writerow(row)
+
 mostTweetsPerHour()
