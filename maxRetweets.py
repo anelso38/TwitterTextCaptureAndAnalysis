6df805eabe878759b7a84c623166c20c41e9d0ae
anelso38 <anelso38@uncc.edu>
2018-04-10 14:33:47 -0400

    Update maxRetweets.py

--- maxRetweets.py
+++ maxRetweets.py
@@ -1,34 +1,28 @@
 import string
 import operator
 import sys
+import csv
 
 def maxRetweets():
-    INPUTFILE = input("Enter the file path: ")
-    INPUT_FILE_PATH = 'C:/Users/##/Desktop/' + INPUTFILE + '.txt' ##ADD USER FILE PATH
+    FilePath = 'C:/Users/Alex Nelson/Desktop/twitterData.txt' 
+    output = []
+    with open (FilePath, encoding = "latin-1") as twitterFile:
+        tweets = twitterFile.readlines()
+          
     
-    with open (INPUT_FILE_PATH, encoding = "latin-1") as myFile:
-        twitter=myFile.readlines()
-
-    l = {}
-
-    for dat in twitter:
-        fileTemp = dat.split()
-        y = len(fileTemp)-2
-        tweet = "\""
-        for x in range (4, y):
-            tweet += fileTemp[x] + " "
-        tweet += " ::::;:::: " + fileTemp[0]
-
-        if tweet not in l:
-            l[tweet] = int(fileTemp[-1])
-
-    outputFile = open('C:/Users/##/Desktop/maxRetweets.txt', 'w', encoding = "utf-8") ##ADD USER FILE PATH
-    l = sorted(l.items(), key = operator.itemgetter(1), reverse = True)
-    outputFile.write("The top 10 tweets that has the max no of retweets : " +"\n\n")
-
-    for x in range (0, 10):
-        outputFile.write(str(x+1) + ". Username: " + l[x][0].split()[-1] + "\n Tweet: " + 
-                         l[x][0].split("::::;::::")[0] + "\n No of retweets: " + str(l[x][1]) + "\n\n")
-    outputFile.close
+    for tweet in tweets:
+        tweetInfo = tweet.split(' ')
+        numberRetweets = int(tweetInfo[-1])
+        info = (tweetInfo[0], numberRetweets)
+        output.append(info)
+        
+    sortedList = sorted(output, key=lambda tup:(tup[1]), reverse = True)
+    maxTen = (sortedList[:10])
     
+    maxRetweetsFile = open('C:/Users/Alex Nelson/Desktop/maxRetweets.csv', 'w')  
+    with maxRetweetsFile:  
+        writer = csv.writer(maxRetweetsFile)
+        for row in maxTen:
+            writer.writerow(row)
+
 maxRetweets()
