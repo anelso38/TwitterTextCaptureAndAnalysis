8626fad5de95cf85389cb5b5f83a7385385353d8
anelso38 <anelso38@uncc.edu>
2018-04-10 14:32:37 -0400

    Update maxFollowers.py

--- maxFollowers.py
+++ maxFollowers.py
@@ -1,27 +1,30 @@
 import string
 import operator
 import sys
+import csv
 
 def maxFollowers():
-    INPUTFILE = input("Enter the file path: ")
-    INPUT_FILE_PATH = 'C:/Users/##/Desktop/' + INPUTFILE + '.txt' ##ADD USER FILE PATH
+    FilePath = 'C:/Users/Alex Nelson/Desktop/twitterData.txt' 
+    output = []
+    with open (FilePath, encoding = "latin-1") as twitterFile:
+        tweets = twitterFile.readlines()
+          
+    
+    for tweet in tweets:
+        tweetInfo = tweet.split(' ')
+        numberFollowers = int(tweetInfo[-2])
+        info = (tweetInfo[0], numberFollowers)
+        output.append(info)
 
-    with open (INPUT_FILE_PATH, encoding = "latin-1") as myFile:
-        twitter=myFile.readlines()
-
-    l = {}
-
-    for dat in twitter:
-        fileTemp = dat.split()
-        if fileTemp[0] not in l:
-            l[fileTemp[0]] = int(fileTemp[-2])
-
-    l = sorted(l.items(), key = operator.itemgetter(1), reverse = True)
-    outputFile = open('C:/Users/##/Desktop/maxFollowers.txt', 'w', encoding = "utf-8") ##ADD USER FILE PATH
-    outputFile.write("The top 10 users who has the maximum followers: " + "\n\n")
-
-    for i in range (0, 10):
-        outputFile.write(str(i+1) + ". Username: " + l[i][0] + " -> Number of Followers: " + str(l[i][1]) + "\n\n")
-    outputFile.close
+        
+    sortedList = sorted(output, key=lambda tup:(tup[1]), reverse=True)
+    
+    maxTen = (sortedList[:10])
+    
+    maxFollowersFile = open('C:/Users/Alex Nelson/Desktop/maxFollowers.csv', 'w')  
+    with maxFollowersFile:  
+        writer = csv.writer(maxFollowersFile)
+        for row in maxTen:
+            writer.writerow(row)
 
 maxFollowers()
